c5ff13c77c143dab978625108750e0d8c7dd4539
dyr11 <1457861939@qq.com>
2016-09-25 17:58:59 +0800

    nowcoder剑指offer mirror tree

--- java/myUtil/src/nowcode/MirrorTree.java
+++ java/myUtil/src/nowcode/MirrorTree.java
@@ -7,6 +7,12 @@ public class MirrorTree {
 }
 class Solution13 {
     public void Mirror(TreeNode root) {
-
+        if(root==null)
+            return ;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        Mirror(root.left);
+        Mirror(root.right);
     }
 }
